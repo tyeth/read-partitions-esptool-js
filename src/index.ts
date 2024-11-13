9509d35f4e9686af96e05e341905275317498314
tyeth <tyethgundry@googlemail.com>
2024-11-13 23:37:05 +0000

    Set Partition Table to be only when connected + DL

--- src/index.ts
+++ src/index.ts
@@ -8,4 +8,9 @@ export {
 } from "./reset.js";
 export { ROM } from "./targets/rom.js";
 export { Transport, SerialOptions } from "./webserial.js";
-export { PartitionDefinition, Partitions } from "./gen_esp32part.py.js";
+export {
+  PartitionDefinition,
+  Partitions,
+  TYPES as PARTITION_TYPES,
+  SUBTYPES as PARTITION_SUBTYPES,
+} from "./gen_esp32part.py.js";
